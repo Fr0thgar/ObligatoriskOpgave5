8db8841199b53b131e1413e6360e68fc86345d11
Fr0thgar <jona270r@edu.easj.dk>
2019-10-02 09:50:10 +0200

    Add project files.

---
+++ ObligatoriskOpgave5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObligatoriskOpgave5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
